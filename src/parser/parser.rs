88683c5f83806317cd3a560ed685a1a51e27d83f
yasinldev <ozkayayasin964@gmail.com>
2024-06-27 00:22:50 +0300

    Update: TokenKind::While now working!

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,4 +1,5 @@
 use crate::lexer::tokens::{Ident, IdentKind, Token, TokenKind, Value};
+use crate::parser::parser::Expr::Builtin;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expr<'a> {
@@ -27,32 +28,11 @@ pub fn generate_expr_tree(token_stream: Vec<Token>) -> Vec<Expr> {
             TokenKind::Ident(ref ident_kind) => {
                 if let Some(value) = token.value {
                     expr_tree.push(
-                        Expr::Builtin(Ident {
+                        Builtin(Ident {
                             kind: IdentKind::Var,
                             value: Some(value),
                         })
                     )
-                } else {
-                    println!("Unexpected token: {:?}", token);
-                }
-            },
-            TokenKind::OpenParen => {
-                if let Some(expr) = parse_expr(&mut token_iter) {
-                    let mut params = Vec::new();
-                    while let Some(next_token) = token_iter.peek().cloned() {
-                        if next_token.kind == TokenKind::CloseParen {
-                            token_iter.next();
-                            break;
-                        } else {
-                            let param_expr = parse_expr(&mut token_iter);
-                            if let Some(param_expr) = param_expr {
-                                params.push(param_expr);
-                            } else {
-                                println!("Unexpected token: {:?}", next_token);
-                            }
-                        }
-                    }
-                    expr_tree.push(Expr::CallParams(Box::new(expr), params));
                 } else {
                     println!("Unexpected token: {:?}", token);
                 }
@@ -91,6 +71,7 @@ pub fn generate_expr_tree(token_stream: Vec<Token>) -> Vec<Expr> {
 fn parse_expr<'a, I>(token_iter: &mut std::iter::Peekable<I>) -> Option<Expr<'a>>
     where I: Iterator<Item = Token<'a>> {
     if let Some(token) = token_iter.next() {
+        println!("Token: {:?}", token.kind);
         match token.kind {
             TokenKind::Literal(ref literal_kind) => {
                 if let Some(value) = token.value {
@@ -101,7 +82,7 @@ fn parse_expr<'a, I>(token_iter: &mut std::iter::Peekable<I>) -> Option<Expr<'a>
             },
             TokenKind::Ident(ref ident_kind) => {
                 if let Some(value) = token.value {
-                    Some(Expr::Builtin(Ident {
+                    Some(Builtin(Ident {
                         kind: IdentKind::Var,
                         value: Some(value)
                     }))
@@ -109,6 +90,27 @@ fn parse_expr<'a, I>(token_iter: &mut std::iter::Peekable<I>) -> Option<Expr<'a>
                     panic!("Unexpected token: {:?}", token);
                 }
             },
+            TokenKind::OpenParen => {
+                if let Some(expr) = parse_expr(token_iter) {
+                    let mut params = Vec::new();
+                    while let Some(next_token) = token_iter.peek().cloned() {
+                        if next_token.kind == TokenKind::CloseParen {
+                            token_iter.next();
+                            break;
+                        } else {
+                            let param_expr = parse_expr(token_iter);
+                            if let Some(param_expr) = param_expr {
+                                params.push(param_expr);
+                            } else {
+                                panic!("Unexpected token: {:?}", next_token);
+                            }
+                        }
+                    }
+                    Some(Expr::CallParams(Box::new(expr), params))
+                } else {
+                    panic!("Unexpected token: {:?}", token);
+                }
+            }
             _ => {
                 println!("Raw Token: {:?}", token);
                 None
